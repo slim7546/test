33b4a27bd4d2545773ae1a04e148533db72290d0
Ivan <example@gmail.com>
2019-10-21 11:24:15 +0300

    это наш первый комит

---
+++ project/script.js
@@ -0,0 +1,30 @@
+'use strict';
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+var obj = {};
+
+
+
+
+// alert("Hello world!");
+
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+// let answer =  prompt("Есть ли Вам 18??", "Да");
+// console.log(typeof(arr));
+let incr = 10,
+decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = false;
+console.log(isChecked || !isClose);
